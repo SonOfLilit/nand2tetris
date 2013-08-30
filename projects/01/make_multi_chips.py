d6de1599aa4bf845b5ce52e26c9417bb697ecc44
Aur Saraf <sonoflilit@gmail.com>
2013-08-30 19:25:25 +0300

    Mux16

--- projects/01/make_multi_chips.py
+++ projects/01/make_multi_chips.py
@@ -1,33 +1,92 @@
 SIZE = 16
-GATES = ["And", "Or"]
-
-FILENAME_TEMPLATE = "%(gate)s%(size)d.hdl"
-
-LINE_TEMPLATE = "    %(gate)s(a=a[%(i)d], b=b[%(i)d], out=out[%(i)d]);"
-TEMPLATE = """// This file is part of www.nand2tetris.org
+FILENAME = "%(name)s.hdl"
+NEWLINE = "\n"
+FILE = """// This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/01/%(gate)s%(size)s.hdl
+// File name: projects/01/%(name)s.hdl
 
-/**
- * %(size)d-bit-wise %(gate)s gate: for i = 0..SIZE: out[i] = a[i] %(gate)s b[i]
- */
-CHIP %(gate)s%(size)d {
-    IN a[%(size)d], b[%(size)d];
-    OUT out[%(size)d];
+CHIP %(name)s {
+%(inputs_code)s
+%(outputs_code)s
 
     PARTS:
-%(code)s
+%(body_code)s
 }
 """
+HEADER_LINE = "    %(keyword)s %(items)s;"
+BODY_LINE = "    %(gate)s(%(inputs)s, %(outputs)s);"
+
+class Gate(object):
+    def __init__(self, gate, size, wide_inputs, single_inputs, outputs):
+        self.gate = gate
+        self.size = size
+        self.wide_inputs = wide_inputs
+        self.single_inputs = single_inputs
+        self.outputs = outputs
+
+    def items(self, *names):
+        """
+        Utility function to generate sub-expressions and return a dict.
+        """
+        return dict((name, getattr(self, name)) for name in names)
+
+    @property
+    def chip(self):
+        return FILE % self.items("name", "inputs_code", "outputs_code", "body_code")
+
+    @property
+    def filename(self):
+        return FILENAME % self.items("name")
+
+    @property
+    def name(self):
+        return "%(gate)s%(size)d" % self.items("gate", "size")
+
+    @property
+    def inputs_code(self):
+        return self.header("IN", self.all_inputs(str(self.size)))
+
+    @property
+    def outputs_code(self):
+        return self.header("OUT", self.all_outputs(str(self.size)))
+
+    def all_inputs(self, index_string):
+        return self.indexed(self.wide_inputs, index_string) + self.indexed(self.single_inputs)
+
+    def all_outputs(self, index_string):
+        return self.indexed(self.outputs, index_string)
+
+    def indexed(self, items, index_string=None):
+        if index_string is None:
+            index_string = ""
+        else:
+            index_string = "[" + index_string + "]"
+        return [(name, "%s%s" % (name, index_string)) for name in items]
+
+    def header(self, keyword, items):
+        return HEADER_LINE % dict(keyword=keyword, items=", ".join(item[1] for item in items))
+
+    @property
+    def body_code(self):
+        gate = self.gate
+        inputs = self.connections(self.all_inputs("%(i)d"))
+        outputs = self.connections(self.all_outputs("%(i)d"))
+        return NEWLINE.join(BODY_LINE % dict(gate=gate,
+                                             inputs=inputs % dict(i=i),
+                                             outputs=outputs % dict(i=i))
+                            for i in xrange(self.size))
+
+    def connections(self, items):
+        return ", ".join("%s=%s" % (n, i) for n, i in items)
+
 
 def write_chip(gate):
-    size = SIZE
-    open(FILENAME_TEMPLATE % locals(), "wb").write(generate_chip(gate, size))
+    open(gate.filename, "wb").write(gate.chip)
 
-def generate_chip(gate, size):
-    code = "\n".join(LINE_TEMPLATE % dict(gate=gate, i=i) for i in xrange(size))
-    return TEMPLATE % locals()
+GATES = [Gate("And", SIZE, ["a", "b"], [], ["out"]),
+         Gate("Or", SIZE, ["a", "b"], [], ["out"]),
+         Gate("Mux", SIZE, ["a", "b"], ["sel"], ["out"])]
 
 if __name__ == '__main__':
     for gate in GATES:
