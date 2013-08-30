f9ada9f003f61424a9dddf911683869c5028b6b6
Aur Saraf <sonoflilit@gmail.com>
2013-08-31 02:24:52 +0300

    Mult, Fill

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+@sum
+M=0
+
+@R0
+D=M
+@counter
+M=D
+
+(LOOP)
+@counter
+D=M
+@END
+D;JLE
+
+@sum
+D=M
+@R1
+D=D+M
+@sum
+M=D
+
+@counter
+M=M-1
+
+@LOOP
+0;JMP
+
+(END)
+@sum
+D=M
+@R2
+M=D
+
+(HALT)
+@HALT
+0;JMP
