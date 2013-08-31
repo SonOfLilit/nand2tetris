48e20b195e640e0d1fd138609c9ff7904ba8fbb0
Aur Saraf <sonoflilit@gmail.com>
2013-08-31 06:43:35 +0300

    parse symbols

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,9 +1,25 @@
+import re
+
+SYMBOL_RE = re.compile("[a-zA-Z_.$:][a-zA-Z0-9_.$:]*")
+
+
+class SyntaxError(Exception):
+    pass
+
 class Literal(object):
     def __repr__(self):
         return "%s(%r)" % (self.__class__.__name__, self.value)
 
 class NumericLiteral(Literal):
     def __init__(self, value):
+        if not (0 <= value < 2 ** 16):
+            raise SyntaxError("Literal out of range: %d" % value)
+        self.value = value
+
+class SymbolLiteral(Literal):
+    def __init__(self, value):
+        if not SYMBOL_RE.match(value):
+            raise SyntaxError("Invalid symbol: %s" % value)
         self.value = value
 
 def parser(text):
@@ -12,25 +28,45 @@ def parser(text):
     []
     >>> list(parser("@0"))
     [NumericLiteral(0)]
-    >>> list(parser(" @0"))
+    >>> list(parser("@65535"))
+    [NumericLiteral(65535)]
+    >>> list(parser("@65536"))
+    Traceback (most recent call last):
+    ...
+    SyntaxError: Literal out of range: 65536
+    >>> list(parser(" @0 "))
     [NumericLiteral(0)]
     >>> list(parser("@a"))
-    [SymbolLiteral("a")]
+    [SymbolLiteral('a')]
+    >>> list(parser("@-"))
+    Traceback (most recent call last):
+    ...
+    SyntaxError: Invalid symbol: -
     >>> list(parser("@a\\n@a"))
-    [SymbolLiteral("a"), SymbolLiteral("a")]
-    >>> list(parser("@a @b //@c"))
-    [SymbolLiteral("a"), SymbolLiteral("b")]
+    [SymbolLiteral('a'), SymbolLiteral('a')]
+    >>> list(parser("@a\\n@b //@c"))
+    [SymbolLiteral('a'), SymbolLiteral('b')]
     >>> list(parser("0;JMP"))
-    [CInstruction(None, "0", "JMP")]
+    [CInstruction(None, '0', 'JMP')]
     >>> list(parser("0;jmp"))
     Traceback:
     ...
     SyntaxError
     """
     for line in text.splitlines():
+        if "//" in line:
+            line = line[:line.find("//")]
         line = line.strip()
-        if line.startswith("@"):
-            yield NumericLiteral(int(line[1:]))
+        if line == "":
+            pass
+        elif line.startswith("@"):
+            literal = line[1:]
+            if literal[0].isdigit():
+                yield NumericLiteral(int(literal))
+            else:
+                yield SymbolLiteral(literal)
+        else:
+            raise SyntaxError("Invalid line: " + line)
 
 if __name__ == '__main__':
     import doctest
