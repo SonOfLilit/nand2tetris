2702cd8ae32bcce784522167555a8c8c399d3104
Aur Saraf <sonoflilit@gmail.com>
2013-08-31 21:06:18 +0300

    parser is better tested and has some bugs fixed

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,24 +1,29 @@
 import re
 
 COMPS = {
-    "0":   "0101010",
-    "1":   "0111111",
-    "-1":  "0111010",
-    "D":   "0001100",
-    "A":   "0110000",
-    "!D":  "0001101",
-    "!A":  "0110001",
-    "-D":  "0001111",
-    "-A":  "0110011",
-    "D+1": "0011111",
-    "A+1": "0011011",
-    "D-1": "0001110",
-    "A+1": "0110010",
-    # TODO: More
+    "0":   "101010",
+    "1":   "111111",
+    "-1":  "111010",
+    "D":   "001100",
+    "A":   "110000",
+    "!D":  "001101",
+    "!A":  "110001",
+    "-D":  "001111",
+    "-A":  "110011",
+    "D+1": "011111",
+    "A+1": "011011",
+    "D-1": "001110",
+    "A-1": "110010",
+    "D+A": "000010",
+    "D-A": "010011",
+    "A-D": "000111",
+    "D&A": "000000",
+    "D|A": "010101"
 }
 for key, value in COMPS.items():
+    COMPS[key] = "0" + value
     if "A" in key:
-        COMPS[key.replace("A", "M")] = "1" + value[1:]
+        COMPS[key.replace("A", "M")] = "1" + value
 
 JUMPS = {
     None:  "000",
@@ -33,7 +38,7 @@ JUMPS = {
 comps = "|".join(re.escape(comp) for comp in COMPS)
 jumps = "|".join(filter(lambda x: x, JUMPS))
 SYMBOL_RE = re.compile("^[a-zA-Z_.$:][a-zA-Z0-9_.$:]*$")
-CINSTRUCTION_RE = re.compile("^(?:(A?M?D?)=|)(%s)(?:|;(%s))$" % (comps, jumps))
+C_INSTRUCTION_RE = re.compile("^(?:(A?M?D?)=|)(%s)(?:|;(%s))$" % (comps, jumps))
 del comps
 del jumps
 
@@ -77,7 +82,7 @@ class CInstruction(Command):
 
     @classmethod
     def parse(cls, line):
-        match = CINSTRUCTION_RE.match(line)
+        match = C_INSTRUCTION_RE.match(line)
         if not match:
             raise SyntaxError("Invalid line: " + line)
         dest, comp, jmp = match.groups()
@@ -114,12 +119,52 @@ def parser(text):
     [CInstruction(None, '0', 'JMP')]
     >>> list(parser("A=A+1"))
     [CInstruction('A', 'A+1', None)]
+    >>> list(parser("D=D+M"))
+    [CInstruction('D', 'D+M', None)]
     >>> list(parser("0;JNE"))
     [CInstruction(None, '0', 'JNE')]
     >>> list(parser("0;jmp"))
     Traceback (most recent call last):
     ...
     SyntaxError: Invalid line: 0;jmp
+    >>> len(list(parser('''@sum
+    ... @sum
+    ... M=0
+    ... 
+    ... @R0
+    ... D=M
+    ... @counter
+    ... M=D
+    ... 
+    ... (LOOP)
+    ... @counter
+    ... D=M
+    ... @END
+    ... D;JLE
+    ... 
+    ... @sum
+    ... D=M
+    ... @R1
+    ... D=D+M
+    ... @sum
+    ... M=D
+    ... 
+    ... @counter
+    ... M=M-1
+    ... 
+    ... @LOOP
+    ... 0;JMP
+    ... 
+    ... (END)
+    ... @sum
+    ... D=M
+    ... @R2
+    ... M=D
+    ... 
+    ... (HALT)
+    ... @HALT
+    ... 0;JMP''')))
+    30
     """
     for line in text.splitlines():
         if "//" in line:
@@ -137,6 +182,11 @@ def parser(text):
             yield Label(line[1:-1])
         else:
             yield CInstruction.parse(line)
+
+
+def code(commands):
+    """
+    """
 
 
 if __name__ == '__main__':
